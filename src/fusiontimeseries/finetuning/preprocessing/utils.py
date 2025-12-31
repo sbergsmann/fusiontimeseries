44c78266ac2532d3b0af63d221b6aa5920f51c01
sbergsmann <29523118+sbergsmann@users.noreply.github.com>
2025-12-31 13:19:43 +0100

    Update data preprocessing utils

--- src/fusiontimeseries/finetuning/preprocessing/utils.py
+++ src/fusiontimeseries/finetuning/preprocessing/utils.py
@@ -1,10 +1,20 @@
 from functools import cache
+import json
 from pathlib import Path
+from typing import Literal
 
 import numpy as np
+from numpy.typing import NDArray
 
-__all__ = ["get_valid_flux_traces"]
+__all__ = ["get_valid_flux_traces", "get_benchmark_flux_traces"]
 
+BENCHMARK_FLUX_DATA_FILE_PATH: Path = (
+    Path(__file__).resolve().parent.parent.parent.parent.parent
+    / "data"
+    / "flux"
+    / "benchmark"
+    / "flux_data.json"
+)
 RAW_FLUX_DATA_PATH: Path = (
     Path(__file__).resolve().parent.parent.parent.parent.parent
     / "data"
@@ -32,6 +42,7 @@ def load_flux_data(idx: int) -> np.ndarray:
     return data[:, 1]  # only energy flux for now, ignore particle and momentum flux
 
 
+@cache
 def get_valid_flux_traces() -> dict[int, np.ndarray]:
     """Get all valid and subsampled flux traces.
 
@@ -63,3 +74,55 @@ def get_valid_flux_traces() -> dict[int, np.ndarray]:
         valid_flux_traces[idx] = subsampled_flux
 
     return valid_flux_traces
+
+
+def get_benchmark_flux_traces() -> dict[
+    Literal["ood", "id"], dict[int, NDArray[np.float32]]
+]:
+    """Get benchmark flux traces for in-distribution and out-of-distribution.
+
+    Returns:
+        dict[str, dict[int, NDArray[np.float32]]]: The dictionary of benchmark flux traces with shape (266,).
+    """
+    flux_data: dict = json.load(open(BENCHMARK_FLUX_DATA_FILE_PATH, "r"))
+
+    benchmark_flux_traces: dict[
+        Literal["ood", "id"], dict[int, NDArray[np.float32]]
+    ] = {}
+
+    ########################################################
+    # In Distribution Benchmark Flux Traces
+    ########################################################
+    in_distribution_iterations: list[str] = [
+        "iteration_8_ifft",
+        "iteration_115_ifft",
+        "iteration_131_ifft",
+        "iteration_148_ifft",
+        "iteration_235_ifft",
+        "iteration_262_ifft",
+    ]
+    benchmark_flux_traces["id"] = {
+        int(iteration.split("_")[1]): np.array(
+            flux_data["in_distribution"][iteration], dtype=np.float32
+        )
+        for iteration in in_distribution_iterations
+    }
+
+    ########################################################
+    # Out Of Distribution Benchmark Flux Traces
+    ########################################################
+    out_of_distribution_iterations: list[str] = [
+        "ood_iteration_0_ifft_realpotens",
+        "ood_iteration_1_ifft_realpotens",
+        "ood_iteration_2_ifft_realpotens",
+        "ood_iteration_3_ifft_realpotens",
+        "ood_iteration_4_ifft_realpotens",
+    ]
+    benchmark_flux_traces["ood"] = {
+        int(iteration.split("_")[2]): np.array(
+            flux_data["out_of_distribution"][iteration], dtype=np.float32
+        )
+        for iteration in out_of_distribution_iterations
+    }
+
+    return benchmark_flux_traces
