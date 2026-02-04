bea46cdeae7b5e43cb037a950baa507ca2ef0ba5
sbergsmann <29523118+sbergsmann@users.noreply.github.com>
2026-02-04 10:35:31 +0100

    Remove legacy namespace

--- src/fusiontimeseries/legacy/__init__.py
+++
@@ -1,4 +0,0 @@
-"""This is a collection of legacy artifacts that developed during experiments.
-
-Files in here are not guaranteed to work out of the box and are certainly not maintained.
-However some parts may be re-activated in future experiments."""
